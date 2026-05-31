2facab546fbbb6b925bd0fa1de85aa0ec398270c
Andrea Izzillo <andrizzillo@gmail.com>
2026-05-31 19:22:58 +0200

    OpenGL context version 4.5

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -10,7 +10,7 @@ Window::Window(int &argc, char **argv, int width, int height, const std::string 
 {
     glutInit(&argc, argv);
 
-    glutInitContextVersion(3, 3);
+    glutInitContextVersion(4, 5);
     glutInitContextProfile(GLUT_CORE_PROFILE);
 
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
